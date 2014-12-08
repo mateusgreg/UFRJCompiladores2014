b2d4fad0a3c7cb9fec85a229e268f2ee7d3a0783
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2014-12-07 23:20:46 -0200

    Scanf funcionando para vetores com indices constantes ou variáveis.

--- compilador/entrada.cc
+++ compilador/entrada.cc
@@ -1,8 +1,7 @@
 int main() {
   
-  int a, i, j;
-  int b[2];
-  float f[10][2];
+  int a, i, j, b[2];
+  float f[10][2], g;
   
   string s;
   string s1[10];
@@ -12,61 +11,49 @@ int main() {
   printf(s);
   
   s = "\nFrase ";
-  s = s + "de" + " teste!";
+  s = s + "de" + " teste!\n";
   printf(s);
   
-  s1[3] = "Mateus";
-  s1[5] = "Gregorio";
-  //printf(s1[3]);
+  s1[3] = "Mateus ";
+  s1[5] = "Gregorio ";
+  printf(s1[3] + "\n");
+  
+  s1[3] = s1[5];
+  printf(s1[3] + "\n");
   
   s2[2][5] = "de Souza";
+  printf(s2[2][5] + "\n");
+  
+  s = s2[2][5];
+  printf(s);
   
   a = 10;
-  a++;
-  a++;
-  a = a+3;
+  printf("\na = " + a);
   
+  b[1] = 50;
+  a = b[1];
+  printf("\na = " + a);
+  
+  a++;
   printf("\nO valor de 'a' é: " + a + "\n");
   
-  for ( i=0; i<10; i++) {
-    printf("\nEstou no for!!! Iteracao: " + i);
-  }
+  f[7][1] = 3.5;
+  printf("\nf = " + f[7][1]);
+  
+  g = f[7][1];
+  printf("\ng = " + g);
   
   printf("\n");
   
-  i=0;
-  while ( i<10 ) {
-    printf("\nEstou no while!!! Iteracao: " + i);
-    i++;
-  }
+  printf("\n\nEntre com um numero inteiro: ");
+  scanf(b[1]);
+  printf("Você digitou: " + b[1] + "\n");
   
-  printf("\n");
+  printf("\nEntre com um numero real (float): ");
+  scanf(f[7][1]);
+  printf("Você digitou: " + f[7][1] + "\n");
   
-  i=0;
-  do {
-    printf("\nEstou no do-while!!! Iteracao: " + i);
-    i++;
-  }while( i<10 );
-  
-  printf("\n\n");
-  
-  switch(1) {
-    case 1: printf("Case 1\n"); 
-    case 2: printf("Case 2\n"); 
-    default: printf("Default\n");
-  }
-  
-  printf(s + "\n");
-  
-  //printf("\n\nEntre com um numero inteiro: ");
-  //scanf(b[1]);
-  //printf("Você digitou: " + b[1] + "\n");
-  
-  //printf("\nEntre com um numero real (float): ");
-  //scanf(f[7][1]);
-  //printf("Você digitou: " + f[7][1] + "\n");
-  
-  //printf("\nAgora entre com uma palavra: ");
-  //scanf(s);
-  //printf("Você digitou: " + s + "\n");
+  printf("\nAgora entre com uma palavra: ");
+  scanf(s);
+  printf("Você digitou: " + s + "\n");
 }
--- compilador/saida.c
+++ compilador/saida.c
@@ -7,13 +7,6 @@ int main(){
   double x_double;
   float x_float;
 
-  int temp_bool_1;
-  int temp_bool_2;
-  int temp_bool_3;
-  int temp_bool_4;
-  int temp_bool_5;
-  int temp_bool_6;
-  int temp_bool_7;
   int temp_int_1;
   int temp_int_2;
   int temp_int_3;
@@ -28,14 +21,40 @@ int main(){
   int temp_int_12;
   int temp_int_13;
   int temp_int_14;
+  int temp_int_15;
+  int temp_int_16;
+  int temp_int_17;
+  int temp_int_18;
+  int temp_int_19;
+  int temp_int_20;
+  int temp_int_21;
+  int temp_int_22;
+  int temp_int_23;
+  int temp_int_24;
+  int temp_int_25;
+  int temp_int_26;
+  int temp_int_27;
+  int temp_int_28;
+  int temp_int_29;
+  int temp_int_30;
+  int temp_int_31;
+  int temp_int_32;
+  int temp_int_33;
+  int temp_int_34;
+  int temp_int_35;
+  int temp_int_36;
+  int temp_int_37;
+  int temp_int_38;
+  int temp_int_39;
   char temp_string_1[256];
   char temp_string_2[256];
 
   int a;
 int i;
 int j;
-  int b[2];
+int b[2];
   float f[20];
+float g;
   char s[256];
   char s1[2560];
   char s2[5376];
@@ -53,7 +72,7 @@ int j;
 
 
   strncpy( temp_string_2, temp_string_1, 255 );
-  strncat( temp_string_2, " teste!", 255 );
+  strncat( temp_string_2, " teste!\n", 255 );
   temp_string_2[255] = 0;
 
   strncpy( s, temp_string_2, 255 );
@@ -62,87 +81,97 @@ int j;
   printf( "%s", s );
   temp_int_1 = 3 * 256;
   temp_int_2 = temp_int_1 + 255;
-  strncpy( &s1[temp_int_1], "Mateus", 255 );
+  strncpy( &s1[temp_int_1], "Mateus ", 255 );
   s1[temp_int_2] = 0;
 
   temp_int_3 = 5 * 256;
   temp_int_4 = temp_int_3 + 255;
-  strncpy( &s1[temp_int_3], "Gregorio", 255 );
+  strncpy( &s1[temp_int_3], "Gregorio ", 255 );
   s1[temp_int_4] = 0;
 
-  temp_int_5 = 2 * 7;
-  temp_int_6 = temp_int_5 + 5;
-  temp_int_7 = temp_int_6 * 256;
+  temp_int_5 = 3 * 256;
+  printf( "%s", &s1[temp_int_5] );
+  printf( "%s", "\n" );
+  temp_int_6 = 5 * 256;
+  temp_int_7 = 3 * 256;
   temp_int_8 = temp_int_7 + 255;
-  strncpy( &s2[temp_int_7], "de Souza", 255 );
-  s2[temp_int_8] = 0;
+  strncpy( &s1[temp_int_7], &s1[temp_int_6], 255 );
+  s1[temp_int_8] = 0;
 
+  temp_int_9 = 3 * 256;
+  printf( "%s", &s1[temp_int_9] );
+  printf( "%s", "\n" );
+  temp_int_10 = 2 * 7;
+  temp_int_11 = temp_int_10 + 5;
+  temp_int_12 = temp_int_11 * 256;
+  temp_int_13 = temp_int_12 + 255;
+  strncpy( &s2[temp_int_12], "de Souza", 255 );
+  s2[temp_int_13] = 0;
+
+  temp_int_14 = 2 * 7;
+  temp_int_15 = temp_int_14 + 5;
+  temp_int_16 = temp_int_15 * 256;
+  printf( "%s", &s2[temp_int_16] );
+  printf( "%s", "\n" );
+  temp_int_17 = 2 * 7;
+  temp_int_18 = temp_int_17 + 5;
+  temp_int_19 = temp_int_18 * 256;
+  strncpy( s, &s2[temp_int_19], 255 );
+  s[255] = 0;
+
+  printf( "%s", s );
   a = 10;
 
-  temp_int_9 = a + 1;
-  a = temp_int_9;
+  printf( "%s", "\na = " );
+  printf( "%d", a );
+  temp_int_20 = 1;
+  b[temp_int_20] = 50;
 
-  temp_int_10 = a + 1;
-  a = temp_int_10;
+  temp_int_21 = 1;
+  a = b[temp_int_21];
 
-  temp_int_11 = a + 3;
-  a = temp_int_11;
+  printf( "%s", "\na = " );
+  printf( "%d", a );
+  temp_int_22 = a + 1;
+  a = temp_int_22;
 
   printf( "%s", "\nO valor de 'a' é: " );
   printf( "%d", a );
   printf( "%s", "\n" );
-  i = 0;
-L_for_cond_1:
-  temp_bool_1 = i < 10;
-  temp_bool_2 = !temp_bool_1;
-  if( temp_bool_2 ) goto L_for_fim_1;
-  printf( "%s", "\nEstou no for!!! Iteracao: " );
-  printf( "%d", i );
-  temp_int_12 = i + 1;
-  i = temp_int_12;
-  goto L_for_cond_1;
-L_for_fim_1:
+  temp_int_23 = 7 * 2;
+  temp_int_24 = temp_int_23 + 1;
+  f[temp_int_24] = 3.5;
 
+  printf( "%s", "\nf = " );
+  temp_int_26 = 7 * 2;
+  temp_int_27 = temp_int_26 + 1;
+  printf( "%f", f[temp_int_27] );
+  temp_int_29 = 7 * 2;
+  temp_int_30 = temp_int_29 + 1;
+  g = f[temp_int_30];
+
+  printf( "%s", "\ng = " );
+  printf( "%f", g );
   printf( "%s", "\n" );
-  i = 0;
-
-L_while_cond_1:
-  temp_bool_3 = i < 10;
-  temp_bool_4 = !temp_bool_3;
-  if( temp_bool_4 ) goto L_while_fim_1;
-  printf( "%s", "\nEstou no while!!! Iteracao: " );
-  printf( "%d", i );
-  temp_int_13 = i + 1;
-  i = temp_int_13;
-
-  goto L_while_cond_1;
-L_while_fim_1:
-
+  printf( "%s", "\n\nEntre com um numero inteiro: " );
+  temp_int_32 = 1;
+  scanf( "%d", &b[temp_int_32] );
+  printf( "%s", "Você digitou: " );
+  temp_int_33 = 1;
+  printf( "%d", b[temp_int_33] );
   printf( "%s", "\n" );
-  i = 0;
-
-L_do_while_inicio_1:
-  printf( "%s", "\nEstou no do-while!!! Iteracao: " );
-  printf( "%d", i );
-  temp_int_14 = i + 1;
-  i = temp_int_14;
-
-  temp_bool_5 = i < 10;
-  if( temp_bool_5 ) goto L_do_while_inicio_1;
-
-  printf( "%s", "\n\n" );
-  temp_bool_7 = 2 == 1;
-  if( temp_bool_7 ) goto L_if_start_case_2;
-  temp_bool_6 = 1 == 1;
-  if( temp_bool_6 ) goto L_if_start_case_1;
-L_if_start_case_1:
-  printf( "%s", "Case 1\n" );
-L_if_start_case_2:
-  printf( "%s", "Case 2\n" );
-  printf( "%s", "Default\n" );
-
-L_if_fim_switch_1:
-
+  printf( "%s", "\nEntre com um numero real (float): " );
+  temp_int_34 = 7 * 2;
+  temp_int_35 = temp_int_34 + 1;
+  scanf( "%f", &f[temp_int_35] );
+  printf( "%s", "Você digitou: " );
+  temp_int_37 = 7 * 2;
+  temp_int_38 = temp_int_37 + 1;
+  printf( "%f", f[temp_int_38] );
+  printf( "%s", "\n" );
+  printf( "%s", "\nAgora entre com uma palavra: " );
+  scanf( "%s", s );
+  printf( "%s", "Você digitou: " );
   printf( "%s", s );
   printf( "%s", "\n" );
 
