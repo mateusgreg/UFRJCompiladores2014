e997a33f4531b3d8a5a25ea4bf29d5d5bcf23455
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2014-12-08 00:18:15 -0200

    Programa de exemplo nº 2 funcionando (sem funcoes).

--- compilador/entrada_matrizes.cc
+++ compilador/entrada_matrizes.cc
@@ -1,22 +1,51 @@
 int main() {
-  int a[3][4];
-  int b[4][10];
-  int i;
-  int j;
-  
-  i = 2;
-  j = 1;
-  a[i][j] = 5;
+  int a[3][4], b[4][10], c[3][10];
+  int i, j, k, soma;
   
   for (i=0; i < 3; i++) {
     for (j=0; j < 4; j++) {
       a[i][j] = i+j;
     }
+  }  
+  
+  for (i=0; i < 4; i++) {
+    for (j=0; j < 10; j++) {
+      b[i][j] = i*j;
+    }
   }
   
- for (i=0; i < 3; i++) {
+  //Calculo do produto das matrizes:
+  for (i=0; i < 3; i++) {
+    for (j=0; j < 10; j++) {
+      soma = 0;
+      for (k=0; k < 4; k++) {
+        soma = soma + (a[i][k] * b[k][j]);
+      }
+      c[i][j] = soma;
+    }
+  }
+  
+  for (i=0; i < 3; i++) {
     for (j=0; j < 4; j++) {
       printf("a[" + i + "][" + j + "] = " + a[i][j] + "   ");
+    }
+    printf("\n");
+  }
+  
+  printf("\n");
+  
+  for (i=0; i < 4; i++) {
+    for (j=0; j < 10; j++) {
+      printf("b[" + i + "][" + j + "] = " + b[i][j] + "   ");
+    }
+    printf("\n");
+  }
+  
+  printf("\n");
+  
+  for (i=0; i < 3; i++) {
+    for (j=0; j < 10; j++) {
+      printf("c[" + i + "][" + j + "] = " + c[i][j] + "   ");
     }
     printf("\n");
   }
--- compilador/saida_matrizes.c
+++ compilador/saida_matrizes.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int main(){
   int x_int;
   double x_double;
@@ -15,6 +16,24 @@ int main(){
   int temp_bool_6;
   int temp_bool_7;
   int temp_bool_8;
+  int temp_bool_9;
+  int temp_bool_10;
+  int temp_bool_11;
+  int temp_bool_12;
+  int temp_bool_13;
+  int temp_bool_14;
+  int temp_bool_15;
+  int temp_bool_16;
+  int temp_bool_17;
+  int temp_bool_18;
+  int temp_bool_19;
+  int temp_bool_20;
+  int temp_bool_21;
+  int temp_bool_22;
+  int temp_bool_23;
+  int temp_bool_24;
+  int temp_bool_25;
+  int temp_bool_26;
   int temp_int_1;
   int temp_int_2;
   int temp_int_3;
@@ -29,19 +48,41 @@ int main(){
   int temp_int_12;
   int temp_int_13;
   int temp_int_14;
+  int temp_int_15;
+  int temp_int_16;
+  int temp_int_17;
+  int temp_int_18;
+  int temp_int_19;
+  int temp_int_20;
+  int temp_int_21;
+  int temp_int_22;
+  int temp_int_23;
+  int temp_int_24;
+  int temp_int_25;
+  int temp_int_26;
+  int temp_int_27;
+  int temp_int_28;
+  int temp_int_29;
+  int temp_int_30;
+  int temp_int_31;
+  int temp_int_32;
+  int temp_int_33;
+  int temp_int_34;
+  int temp_int_35;
+  int temp_int_36;
+  int temp_int_37;
+  int temp_int_38;
+  int temp_int_39;
+  int temp_int_40;
+  int temp_int_41;
 
   int a[12];
-  int b[40];
+int b[40];
+int c[30];
   int i;
-  int j;
-  i = 2;
-
-  j = 1;
-
-  temp_int_1 = i * 4;
-  temp_int_2 = temp_int_1 + j;
-  a[temp_int_2] = 5;
-
+int j;
+int k;
+int soma;
   i = 0;
 L_for_cond_2:
   temp_bool_1 = i < 3;
@@ -52,50 +93,181 @@ L_for_cond_1:
   temp_bool_2 = j < 4;
   temp_bool_3 = !temp_bool_2;
   if( temp_bool_3 ) goto L_for_fim_1;
-  temp_int_6 = i + j;
-  temp_int_7 = i * 4;
-  temp_int_8 = temp_int_7 + j;
-  a[temp_int_8] = temp_int_6;
+  temp_int_3 = i + j;
+  temp_int_4 = i * 4;
+  temp_int_5 = temp_int_4 + j;
+  a[temp_int_5] = temp_int_3;
 
-  temp_int_5 = j + 1;
-  j = temp_int_5;
+  temp_int_2 = j + 1;
+  j = temp_int_2;
   goto L_for_cond_1;
 L_for_fim_1:
 
-  temp_int_4 = i + 1;
-  i = temp_int_4;
+  temp_int_1 = i + 1;
+  i = temp_int_1;
   goto L_for_cond_2;
 L_for_fim_2:
 
   i = 0;
 L_for_cond_4:
-  temp_bool_5 = i < 3;
+  temp_bool_5 = i < 4;
   temp_bool_8 = !temp_bool_5;
   if( temp_bool_8 ) goto L_for_fim_4;
   j = 0;
 L_for_cond_3:
-  temp_bool_6 = j < 4;
+  temp_bool_6 = j < 10;
   temp_bool_7 = !temp_bool_6;
   if( temp_bool_7 ) goto L_for_fim_3;
+  temp_int_9 = i * j;
+  temp_int_10 = i * 10;
+  temp_int_11 = temp_int_10 + j;
+  b[temp_int_11] = temp_int_9;
+
+  temp_int_8 = j + 1;
+  j = temp_int_8;
+  goto L_for_cond_3;
+L_for_fim_3:
+
+  temp_int_7 = i + 1;
+  i = temp_int_7;
+  goto L_for_cond_4;
+L_for_fim_4:
+
+  i = 0;
+L_for_cond_7:
+  temp_bool_9 = i < 3;
+  temp_bool_14 = !temp_bool_9;
+  if( temp_bool_14 ) goto L_for_fim_7;
+  j = 0;
+L_for_cond_6:
+  temp_bool_10 = j < 10;
+  temp_bool_13 = !temp_bool_10;
+  if( temp_bool_13 ) goto L_for_fim_6;
+  soma = 0;
+
+  k = 0;
+L_for_cond_5:
+  temp_bool_11 = k < 4;
+  temp_bool_12 = !temp_bool_11;
+  if( temp_bool_12 ) goto L_for_fim_5;
+  temp_int_16 = i * 4;
+  temp_int_17 = temp_int_16 + k;
+  temp_int_19 = k * 10;
+  temp_int_20 = temp_int_19 + j;
+  temp_int_22 = a[temp_int_17] * b[temp_int_20];
+  temp_int_23 = soma + temp_int_22;
+  soma = temp_int_23;
+
+  temp_int_15 = k + 1;
+  k = temp_int_15;
+  goto L_for_cond_5;
+L_for_fim_5:
+
+  temp_int_24 = i * 10;
+  temp_int_25 = temp_int_24 + j;
+  c[temp_int_25] = soma;
+
+  temp_int_14 = j + 1;
+  j = temp_int_14;
+  goto L_for_cond_6;
+L_for_fim_6:
+
+  temp_int_13 = i + 1;
+  i = temp_int_13;
+  goto L_for_cond_7;
+L_for_fim_7:
+
+  i = 0;
+L_for_cond_9:
+  temp_bool_15 = i < 3;
+  temp_bool_18 = !temp_bool_15;
+  if( temp_bool_18 ) goto L_for_fim_9;
+  j = 0;
+L_for_cond_8:
+  temp_bool_16 = j < 4;
+  temp_bool_17 = !temp_bool_16;
+  if( temp_bool_17 ) goto L_for_fim_8;
   printf( "%s", "a[" );
   printf( "%d", i );
   printf( "%s", "][" );
   printf( "%d", j );
   printf( "%s", "] = " );
-  temp_int_12 = i * 4;
-  temp_int_13 = temp_int_12 + j;
-  printf( "%d", a[temp_int_13] );
+  temp_int_29 = i * 4;
+  temp_int_30 = temp_int_29 + j;
+  printf( "%d", a[temp_int_30] );
   printf( "%s", "   " );
-  temp_int_11 = j + 1;
-  j = temp_int_11;
-  goto L_for_cond_3;
-L_for_fim_3:
+  temp_int_28 = j + 1;
+  j = temp_int_28;
+  goto L_for_cond_8;
+L_for_fim_8:
 
   printf( "%s", "\n" );
-  temp_int_10 = i + 1;
-  i = temp_int_10;
-  goto L_for_cond_4;
-L_for_fim_4:
+  temp_int_27 = i + 1;
+  i = temp_int_27;
+  goto L_for_cond_9;
+L_for_fim_9:
+
+  printf( "%s", "\n" );
+  i = 0;
+L_for_cond_11:
+  temp_bool_19 = i < 4;
+  temp_bool_22 = !temp_bool_19;
+  if( temp_bool_22 ) goto L_for_fim_11;
+  j = 0;
+L_for_cond_10:
+  temp_bool_20 = j < 10;
+  temp_bool_21 = !temp_bool_20;
+  if( temp_bool_21 ) goto L_for_fim_10;
+  printf( "%s", "b[" );
+  printf( "%d", i );
+  printf( "%s", "][" );
+  printf( "%d", j );
+  printf( "%s", "] = " );
+  temp_int_34 = i * 10;
+  temp_int_35 = temp_int_34 + j;
+  printf( "%d", b[temp_int_35] );
+  printf( "%s", "   " );
+  temp_int_33 = j + 1;
+  j = temp_int_33;
+  goto L_for_cond_10;
+L_for_fim_10:
+
+  printf( "%s", "\n" );
+  temp_int_32 = i + 1;
+  i = temp_int_32;
+  goto L_for_cond_11;
+L_for_fim_11:
+
+  printf( "%s", "\n" );
+  i = 0;
+L_for_cond_13:
+  temp_bool_23 = i < 3;
+  temp_bool_26 = !temp_bool_23;
+  if( temp_bool_26 ) goto L_for_fim_13;
+  j = 0;
+L_for_cond_12:
+  temp_bool_24 = j < 10;
+  temp_bool_25 = !temp_bool_24;
+  if( temp_bool_25 ) goto L_for_fim_12;
+  printf( "%s", "c[" );
+  printf( "%d", i );
+  printf( "%s", "][" );
+  printf( "%d", j );
+  printf( "%s", "] = " );
+  temp_int_39 = i * 10;
+  temp_int_40 = temp_int_39 + j;
+  printf( "%d", c[temp_int_40] );
+  printf( "%s", "   " );
+  temp_int_38 = j + 1;
+  j = temp_int_38;
+  goto L_for_cond_12;
+L_for_fim_12:
+
+  printf( "%s", "\n" );
+  temp_int_37 = i + 1;
+  i = temp_int_37;
+  goto L_for_cond_13;
+L_for_fim_13:
 
 
   return 0;
